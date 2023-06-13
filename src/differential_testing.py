98248e5a2fe5ccb2f563631dfcabdda7c45d572a
Nathee Jaywaree <puntf2@gmail.com>
2023-06-13 21:56:27 +0800

    Add command for postgres, sql

--- src/differential_testing.py
+++ src/differential_testing.py
@@ -4,7 +4,26 @@ import subprocess
 import re
 
 if __name__ == '__main__':
+
     print(sys.platform)
+    logs = None
+    if sys.platform == "win32":
+        logs = subprocess.Popen("powershell -c cat ./target/logs/sqlite3/database0-cur.log", shell=True, stdout=subprocess.PIPE).stdout
+    else:
+        logs = subprocess.Popen("cat ./target/logs/sqlite3/database0-cur.log", shell=True, stdout=subprocess.PIPE).stdout
+   
+    time = logs.readline().decode()
+    database_name = logs.readline().decode().split(':')[1].strip()
+    database_version = logs.readline().decode()
+    database_seed = logs.readline().decode()
+    statements = ""
+    while True:
+        line = logs.readline().decode().strip().split(';')[0]
+        if not line:
+            break
+        statements += line + ';'
+        # print(line)
+
     ps = subprocess.Popen("docker ps", shell=True, stdout=subprocess.PIPE).stdout 
     mysql_id = ""
     postgres_id = ""
@@ -19,32 +38,12 @@ if __name__ == '__main__':
             postgres_id = tokens[0]
     print(mysql_id)
     print(postgres_id)
-
-    logs = subprocess.Popen("powershell -c cat ./target/logs/sqlite3/database0-cur.log", shell=True, stdout=subprocess.PIPE).stdout
-    time = logs.readline().decode()
-    database_name = logs.readline().decode().split(':')[1].strip()
-    database_version = logs.readline().decode()
-    database_seed = logs.readline().decode()
-    statements = ""
-    while True:
-        line = logs.readline().decode().strip().split(';')[0]
-        if not line:
-            break
-        statements += line + ';'
-        # print(line)
-
-    print(statements)
-    s = f"""docker exec -i {mysql_id} sh -c "mysql -uroot -proot -e 'CREATE DATABASE {database_name}; USE {database_name};' " """
-    print(s)
-    subprocess.Popen(s, shell=True, stdout=subprocess.PIPE, stdin=subprocess.PIPE)
-    # mysql = subprocess.Popen(f"""docker exec -i {mysql_id} sh -c "mysql -uroot -proot" -e {statements} """, shell=True, stdout=subprocess.PIPE, stdin=subprocess.PIPE)
-    # print(mysql.stdin.write(b"CREATE TABLE IF NOT EXISTS t0 (c0 TEXT); -- 20ms;"))
-    # print(mysql.stdout.read())
-    # print(mysql.communicate(b"SELECT * FROM t0;")[0])
-    # print(mysql.communicate(b"SHOW DATABASES;")[0])
-    # print(mysql.stdout.read().decode())
-    # mysql = subprocess.Popen(f"powershell", shell=True, stdout=subprocess.PIPE, stdin=subprocess.PIPE)
-    # print(mysql.communicate(b"ls"))
-
     
-    
+    mysql_command = f"""docker exec -i {mysql_id} bash -c "mysql -uroot -proot" """
+    # print(s)
+    mysql = subprocess.Popen(mysql_command, shell=True, stdout=subprocess.PIPE, stdin=subprocess.PIPE)
+    print(mysql.communicate(f"CREATE DATABASE {database_name}; USE {database_name}; {statements}".encode())[0].decode())
+
+    postgres_command = f"""docker exec -i {postgres_id} bash -c "psql -U sqlancer test -c 'CREATE DATABASE {database_name}'; psql -U sqlancer {database_name}" """
+    postgres = subprocess.Popen(postgres_command, shell=True, stdout=subprocess.PIPE, stdin=subprocess.PIPE)
+    print(postgres.communicate(statements.encode())[0].decode())
