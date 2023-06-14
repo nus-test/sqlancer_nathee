ff617c4ffffaa17dd1fd67ff934662d7d77b82b9
eltfshr <puntf2@gmail.com>
2023-06-14 18:03:53 +0800

    Add drop databases

--- src/differential_testing.py
+++ src/differential_testing.py
@@ -5,6 +5,7 @@ import re
 
 if __name__ == '__main__':
 
+    ## get log file
     print(sys.platform)
     logs = None
     if sys.platform == "win32":
@@ -12,6 +13,7 @@ if __name__ == '__main__':
     else:
         logs = subprocess.Popen("cat ./target/logs/sqlite3/database0-cur.log", shell=True, stdout=subprocess.PIPE).stdout
    
+    ## extract information and statements from log file
     time = logs.readline().decode()
     database_name = logs.readline().decode().split(':')[1].strip()
     database_version = logs.readline().decode()
@@ -22,8 +24,8 @@ if __name__ == '__main__':
         if not line:
             break
         statements += line + ';'
-        # print(line)
 
+    ## get docker containers id
     ps = subprocess.Popen("docker ps", shell=True, stdout=subprocess.PIPE).stdout 
     mysql_id = ""
     postgres_id = ""
@@ -39,11 +41,54 @@ if __name__ == '__main__':
     print(mysql_id)
     print(postgres_id)
     
-    mysql_command = f"""docker exec -i {mysql_id} bash -c "mysql -uroot -proot" """
-    # print(s)
-    mysql = subprocess.Popen(mysql_command, shell=True, stdout=subprocess.PIPE, stdin=subprocess.PIPE)
-    print(mysql.communicate(f"CREATE DATABASE {database_name}; USE {database_name}; {statements}".encode())[0].decode())
+    ## drop existing database from mysql
+    drop_command = ""
+    mysql_console = subprocess.Popen(f"""docker exec -i {mysql_id} bash -c "mysql -uroot -proot -sN" """, shell=True, stdout=subprocess.PIPE, stdin=subprocess.PIPE)
+    list_command = f"""SELECT schema_name FROM information_schema.schemata WHERE schema_name LIKE "database%"; """
+    mysql_tables = mysql_console.communicate(list_command.encode())[0].decode().split()
+    for table in mysql_tables:
+        drop_command += f"DROP DATABASE {table};"
+    subprocess.Popen(f"""docker exec {mysql_id} bash -c "mysql -uroot -proot -e '{drop_command}' """, shell=True, stdout=subprocess.PIPE, stdin=subprocess.PIPE)
 
-    postgres_command = f"""docker exec -i {postgres_id} bash -c "psql -U sqlancer test -c 'CREATE DATABASE {database_name}'; psql -U sqlancer {database_name}" """
+    ## drop existing database from postgres
+    drop_command = ""
+    psql_console = subprocess.Popen(f"""docker exec -i {postgres_id} bash -c "psql -t -q -U sqlancer postgres" """, shell=True, stdout=subprocess.PIPE, stdin=subprocess.PIPE)
+    list_command = """ SELECT datname FROM pg_database WHERE datname LIKE 'database%'; """
+    postgres_tables = psql_console.communicate(list_command.encode())[0].decode().split()
+    for table in postgres_tables:
+        drop_command += f"DROP DATABASE {table};"
+    psql_console = subprocess.Popen(f"""docker exec -i {postgres_id} bash -c "psql -t -q -U sqlancer postgres " """, shell=True, stdout=subprocess.PIPE, stdin=subprocess.PIPE)
+    psql_console.communicate(drop_command.encode())
+    
+    ## drop existing database from sqlite
+    if sys.platform == "win32":
+        try:
+            drop_command = f"""del ".\\target\\databases\\database*" """
+        except:
+            pass
+    else:
+        drop_command = f"""rm ./target/databases/database*  """
+    subprocess.Popen(drop_command, shell=True, stdout=subprocess.PIPE, stdin=subprocess.PIPE)
+        
+    ## create database and execute statements on mysql
+    mysql_command = f"""docker exec -i {mysql_id} bash -c "mysql -uroot -proot -sN" """
+    mysql = subprocess.Popen(mysql_command, shell=True, stdout=subprocess.PIPE, stdin=subprocess.PIPE)
+    mysql_result = mysql.communicate(f"CREATE DATABASE {database_name}; USE {database_name}; {statements}".encode())[0].decode()
+    print(mysql_result)
+
+    ## create database and execute statements on postgres
+    postgres_command = f"""docker exec -i {postgres_id} bash -c "psql -q -U sqlancer test -c 'CREATE DATABASE {database_name}'; psql -t -q -U sqlancer {database_name}" """
     postgres = subprocess.Popen(postgres_command, shell=True, stdout=subprocess.PIPE, stdin=subprocess.PIPE)
-    print(postgres.communicate(statements.encode())[0].decode())
+    postgres_result = postgres.communicate(statements.encode())[0].decode()
+    print(postgres_result[:-1])
+
+    ## create database and execute statements on sqlite
+    sqlite_command = f""" sqlite3.exe ./target/databases/{database_name} "{statements}" """
+    sqlite_command = f""" sqlite3.exe ./target/databases/{database_name}.db"""
+    sqlite_console = subprocess.Popen(sqlite_command, shell=True, stdout=subprocess.PIPE, stdin=subprocess.PIPE)
+    sqlite_result = sqlite_console.communicate(statements.encode())[0].decode()
+    print(sqlite_result)
+
+    print(mysql_result == postgres_result[:-1])
+    print(mysql_result == sqlite_result)
+    print(sqlite_result == postgres_result[:-1])
