be384af52799adcd003ac1eb84280234f32b664b
Nathee Jaywaree <puntf2@gmail.com>
2023-11-30 03:28:01 +0700

    Change script to use threads

--- src/differential_testing.py
+++ src/differential_testing.py
@@ -1,3 +1,4 @@
+## run from root of sqlancer: python .\src\differential_testing.py
 import os
 import sys
 import subprocess
@@ -6,6 +7,196 @@ import glob
 import mysql.connector #This needs to be downloaded: pip install mysql-connector-python
 import psycopg #This needs to be downloaded: pip install "psycopg[binary]"
 import sqlite3
+import threading
+
+
+def run_from_file(file):
+    errors = []
+    logs = open(file, 'r')
+    names = re.split(r'[\\/]', logs.name)
+    # extract information and statements from log file from python lib
+    time = logs.readline()
+    database_name = names[len(names) - 2][0] + logs.readline().split(':')[1].strip() # prefix with databases' first letter
+    database_version = logs.readline()
+    database_seed = logs.readline()
+    statements = ""
+    select_statements = list()
+    while True:
+        line = logs.readline().strip().split(';')[0]
+        if not line:
+            break
+        if line.upper().startswith("SELECT"):
+            select_statements.append(line + ';')
+        else:
+            statements += line + ';'
+
+
+    # ## drop existing database from mysql with command line
+    # drop_command = ""
+    # mysql_console = subprocess.Popen(f"""docker exec -i {mysql_id} bash -c "mysql -uroot -proot -sN" """, shell=True, stdout=subprocess.PIPE, stdin=subprocess.PIPE)
+    # list_command = f"""SELECT schema_name FROM information_schema.schemata WHERE schema_name LIKE "database%"; """
+    # mysql_databases = mysql_console.communicate(list_command.encode())[0].decode().split()
+    # for database in mysql_databases:
+    #     drop_command += f"DROP DATABASE {database};"
+    # subprocess.Popen(f"""docker exec {mysql_id} bash -c "mysql -uroot -proot -e '{drop_command}' """, shell=True, stdout=subprocess.PIPE, stdin=subprocess.PIPE)
+    #
+    # ## drop existing database from postgres with command line
+    # drop_command = ""
+    # psql_console = subprocess.Popen(f"""docker exec -i {postgres_id} bash -c "psql -t -q -U sqlancer postgres" """, shell=True, stdout=subprocess.PIPE, stdin=subprocess.PIPE)
+    # list_command = """ SELECT datname FROM pg_database WHERE datname LIKE 'database%'; """
+    # postgres_databases = psql_console.communicate(list_command.encode())[0].decode().split()
+    # for database in postgres_databases:
+    #     drop_command += f"DROP DATABASE {database};"
+    # psql_console = subprocess.Popen(f"""docker exec -i {postgres_id} bash -c "psql -t -q -U sqlancer postgres " """, shell=True, stdout=subprocess.PIPE, stdin=subprocess.PIPE)
+    # psql_console.communicate(drop_command.encode())
+    #
+    ## drop existing database from sqlite with command line
+    # if sys.platform == "win32":
+    #     try:
+    #         drop_command = f"""del ".\\target\\databases\\database*" """
+    #     except:
+    #         pass
+    # else:
+    #     drop_command = f"""rm ./target/databases/database*  """
+    # subprocess.Popen(drop_command, shell=True, stdout=subprocess.PIPE, stdin=subprocess.PIPE)
+        
+        
+    ## drop existing database from mysql with python lib
+    mysql_con = mysql.connector.connect(user="root", password="root")
+    mysql_cur = mysql_con.cursor()
+    mysql_cur.execute(f"DROP DATABASE IF EXISTS {database_name};")
+    # mysql_cur.execute("""SELECT schema_name FROM information_schema.schemata WHERE schema_name LIKE 'database%'; """)
+    # for res in mysql_cur.fetchall():
+    #     mysql_cur.execute(f"DROP DATABASE {res[0]};")
+
+    ## drop existing database from postgres with python lib
+    postgres_con = psycopg.connect("dbname=postgres user=sqlancer password=sqlancer")
+    postgres_con.autocommit = True
+    postgres_cur = postgres_con.cursor()
+    postgres_cur.execute(f"DROP DATABASE IF EXISTS {database_name};")
+    # postgres_cur.execute("SELECT datname FROM pg_database WHERE datname LIKE 'database%';")
+    # for res in postgres_cur.fetchall():
+    #     postgres_cur.execute(f"DROP DATABASE {res[0]};")
+
+    ## drop existing database from sqlite with python lib
+    try:
+        os.remove(f"./target/databases/{database_name}.db")
+    except:
+        pass
+    # sqlite_databases = glob.glob("./target/databases/database*")
+    # for database in sqlite_databases:
+    #     os.remove(database)
+
+
+    # ## create database and execute statements on mysql with command line
+    # mysql_command = f"""docker exec -i {mysql_id} bash -c "mysql -uroot -proot -sN" """
+    # mysql = subprocess.Popen(mysql_command, shell=True, stdout=subprocess.PIPE, stdin=subprocess.PIPE)
+    # mysql_result = mysql.communicate(f"CREATE DATABASE {database_name}; USE {database_name}; {statements}".encode())[0].decode()
+    #
+    # ## create database and execute statements on postgres with command line
+    # postgres_command = f"""docker exec -i {postgres_id} bash -c "psql -q -U sqlancer test -c 'CREATE DATABASE {database_name}'; psql -t -q -U sqlancer {database_name}" """
+    # postgres = subprocess.Popen(postgres_command, shell=True, stdout=subprocess.PIPE, stdin=subprocess.PIPE)
+    # postgres_result = postgres.communicate(statements.encode())[0].decode()
+    #
+    # ## create database and execute statements on sqlite with command line
+    # sqlite_command = f""" sqlite3.exe ./target/databases/{database_name}.db"""
+    # sqlite_console = subprocess.Popen(sqlite_command, shell=True, stdout=subprocess.PIPE, stdin=subprocess.PIPE)
+    # sqlite_result = sqlite_console.communicate(statements.encode())[0].decode()
+
+
+    ## create database and execute statements on mysql with python lib
+    mysql_list = list()
+    mysql_set = set()
+    try:
+        mysql_cur.execute(f"CREATE DATABASE {database_name};")
+        mysql_con = mysql.connector.connect(user="root", password="root", database=database_name)
+        mysql_con.autocommit = True #not actually needed
+        mysql_cur = mysql_con.cursor()
+        mysql_cur.execute(statements, multi=True)
+        for cur in mysql_cur.execute(statements, multi=True):
+            pass
+        for select in select_statements:
+            mysql_cur.execute(select)
+            for res in mysql_cur.fetchall():
+                mysql_list.append(res[0])
+                mysql_set.add(res[0])
+                print("MySQL: " + res[0])
+            print("\n")
+    except mysql.connector.Error as e: #mysql stop executing after finding an error
+        errors.append({
+            "where": f"\nMySQL error at {database_name}:",
+            "error": e,
+            "syntax": "sql syntax" in str(e).lower()
+        })
+
+    ## create database and execute statements on postgres with python lib
+    postgres_list = list()
+    postgres_set = set()
+    try:
+        postgres_cur.execute(f"CREATE DATABASE {database_name};")
+        postgres_con = psycopg.connect(f"dbname={database_name} user=sqlancer password=sqlancer")
+        postgres_con.autocommit = True
+        postgres_cur = postgres_con.cursor()
+        postgres_cur.execute(statements)
+        for select in select_statements:
+            postgres_cur.execute(select)
+            for res in postgres_cur.fetchall():
+                postgres_list.append(res[0])
+                postgres_set.add(res[0])
+                print("PostgreSQL: " + res[0])
+            print("\n")
+    except psycopg.Error as e: #postgres will not execute from the start if an error is detected
+        errors.append({
+            "where": f"\nPostgreSQL error at {database_name}:",
+            "error": e,
+            "syntax": "syntax error" in str(e).lower()
+        })
+
+    ## create database and execute statements on sqlite with python lib
+    sqlite_list = list()
+    sqlite_set = set()
+    try:
+        sqlite_con = sqlite3.connect(f"./target/databases/{database_name}.db")
+        sqlite_cur = sqlite_con.cursor()
+        res = sqlite_cur.executescript(statements)
+        for select in select_statements:
+            res = sqlite_cur.execute(select)
+            for tuple in res.fetchall():
+                sqlite_list.append(tuple[0])
+                sqlite_set.add(tuple[0])
+                print("SQLite: " + tuple[0])
+            print("\n")
+    except sqlite3.Error as e: #sqlite stop executing after finding an error
+        errors.append({
+            "where": f"\nSQLite error at {database_name}: ",
+            "error": e,
+            "syntax": "syntax error" in str(e).lower()
+        })
+
+    # print("Check number of rows")
+    # print(len(mysql_list) == len(postgres_list), len(sqlite_list) == len(postgres_list), len(mysql_list) == len(sqlite_list))
+    # len = len(sqlite_list)
+    # # for i in range(len):
+    # #     print("'" + mysql_list[i] + "'")
+    # #     print("'" + postgres_list[i] + "'")
+    # #     print("'" + sqlite_list[i] + "'")
+    
+    # for element in mysql_set:
+    #     print("'" + element + "'")
+    # for element in postgres_set:
+    #     print("'" + element + "'")
+    # for element in sqlite_set:
+    #     print("'" + element + "'")
+
+    # print("Check content of rows")
+    # print(mysql_set == postgres_set, postgres_set == sqlite_set, mysql_set == sqlite_set)
+
+    # mysql always escape \ but the other two do not. SET GLOBAL/SESSION sql_mode = 'NO_BACKSLASH_ESCAPES'; mode can disable it
+    for error in errors:
+        print(error["where"])
+        print(error["error"])
+        if error["syntax"]:
+            print("^-----------------------------------Syntax error-----------------------------------^")
 
 
 if __name__ == '__main__':
@@ -35,174 +226,31 @@ if __name__ == '__main__':
     #         statements += line + ';'
 
 
-    ## get docker containers id
-    ps = subprocess.Popen("docker ps", shell=True, stdout=subprocess.PIPE).stdout 
-    mysql_id = ""
-    postgres_id = ""
-    while True:
-        line = ps.readline().decode()
-        if not line:
-            break
-        tokens = line.split()
-        if tokens[1] == "mysql":
-            mysql_id = tokens[0]
-        if tokens[1] == "postgres":
-            postgres_id = tokens[0]
-    print(mysql_id)
-    print(postgres_id)
-
+    # ## get docker containers id with command line
+    # ps = subprocess.Popen("docker ps", shell=True, stdout=subprocess.PIPE).stdout 
+    # mysql_id = ""
+    # postgres_id = ""
+    # while True:
+    #     line = ps.readline().decode()
+    #     if not line:
+    #         break
+    #     tokens = line.split()
+    #     if tokens[1] == "mysql":
+    #         mysql_id = tokens[0]
+    #     if tokens[1] == "postgres":
+    #         postgres_id = tokens[0]
+    # print(mysql_id)
+    # print(postgres_id)
 
     ## get log files with python lib
     log_files = glob.glob("./target/logs/*/database*")
+    threads = []
     for file in log_files:
-        logs = open(file, 'r')
+        t = threading.Thread(target=run_from_file, args=[file])
+        threads.append(t)
+        t.start()
+    
+    for t in threads:
+        t.join()
 
-        # extract information and statements from log file from python lib
-        time = logs.readline()
-        database_name = logs.readline().split(':')[1].strip()
-        database_version = logs.readline()
-        database_seed = logs.readline()
-        statements = ""
-        select_statements = list()
-        while True:
-            line = logs.readline().strip().split(';')[0]
-            if not line:
-                break
-            if line.upper().startswith("SELECT"):
-                select_statements.append(line + ';')
-            else:
-                statements += line + ';'
-
-
-        # ## drop existing database from mysql with command line
-        # drop_command = ""
-        # mysql_console = subprocess.Popen(f"""docker exec -i {mysql_id} bash -c "mysql -uroot -proot -sN" """, shell=True, stdout=subprocess.PIPE, stdin=subprocess.PIPE)
-        # list_command = f"""SELECT schema_name FROM information_schema.schemata WHERE schema_name LIKE "database%"; """
-        # mysql_databases = mysql_console.communicate(list_command.encode())[0].decode().split()
-        # for database in mysql_databases:
-        #     drop_command += f"DROP DATABASE {database};"
-        # subprocess.Popen(f"""docker exec {mysql_id} bash -c "mysql -uroot -proot -e '{drop_command}' """, shell=True, stdout=subprocess.PIPE, stdin=subprocess.PIPE)
-        #
-        # ## drop existing database from postgres with command line
-        # drop_command = ""
-        # psql_console = subprocess.Popen(f"""docker exec -i {postgres_id} bash -c "psql -t -q -U sqlancer postgres" """, shell=True, stdout=subprocess.PIPE, stdin=subprocess.PIPE)
-        # list_command = """ SELECT datname FROM pg_database WHERE datname LIKE 'database%'; """
-        # postgres_databases = psql_console.communicate(list_command.encode())[0].decode().split()
-        # for database in postgres_databases:
-        #     drop_command += f"DROP DATABASE {database};"
-        # psql_console = subprocess.Popen(f"""docker exec -i {postgres_id} bash -c "psql -t -q -U sqlancer postgres " """, shell=True, stdout=subprocess.PIPE, stdin=subprocess.PIPE)
-        # psql_console.communicate(drop_command.encode())
-        #
-        ## drop existing database from sqlite with command line
-        # if sys.platform == "win32":
-        #     try:
-        #         drop_command = f"""del ".\\target\\databases\\database*" """
-        #     except:
-        #         pass
-        # else:
-        #     drop_command = f"""rm ./target/databases/database*  """
-        # subprocess.Popen(drop_command, shell=True, stdout=subprocess.PIPE, stdin=subprocess.PIPE)
-            
-            
-        ## drop existing database from mysql with python lib
-        mysql_con = mysql.connector.connect(user="root", password="root")
-        mysql_cur = mysql_con.cursor()
-        mysql_cur.execute(f"DROP DATABASE IF EXISTS {database_name};")
-        # mysql_cur.execute("""SELECT schema_name FROM information_schema.schemata WHERE schema_name LIKE 'database%'; """)
-        # for res in mysql_cur.fetchall():
-        #     mysql_cur.execute(f"DROP DATABASE {res[0]};")
-
-        ## drop existing database from postgres with python lib
-        postgres_con = psycopg.connect("dbname=postgres user=sqlancer password=sqlancer")
-        postgres_con.autocommit = True
-        postgres_cur = postgres_con.cursor()
-        postgres_cur.execute(f"DROP DATABASE IF EXISTS {database_name};")
-        # postgres_cur.execute("SELECT datname FROM pg_database WHERE datname LIKE 'database%';")
-        # for res in postgres_cur.fetchall():
-        #     postgres_cur.execute(f"DROP DATABASE {res[0]};")
-
-        ## drop existing database from sqlite with python lib
-        try:
-            os.remove(f"./target/databases/{database_name}.db")
-        except:
-            pass
-        # sqlite_databases = glob.glob("./target/databases/database*")
-        # for database in sqlite_databases:
-        #     os.remove(database)
-
-
-        # ## create database and execute statements on mysql with command line
-        # mysql_command = f"""docker exec -i {mysql_id} bash -c "mysql -uroot -proot -sN" """
-        # mysql = subprocess.Popen(mysql_command, shell=True, stdout=subprocess.PIPE, stdin=subprocess.PIPE)
-        # mysql_result = mysql.communicate(f"CREATE DATABASE {database_name}; USE {database_name}; {statements}".encode())[0].decode()
-        #
-        # ## create database and execute statements on postgres with command line
-        # postgres_command = f"""docker exec -i {postgres_id} bash -c "psql -q -U sqlancer test -c 'CREATE DATABASE {database_name}'; psql -t -q -U sqlancer {database_name}" """
-        # postgres = subprocess.Popen(postgres_command, shell=True, stdout=subprocess.PIPE, stdin=subprocess.PIPE)
-        # postgres_result = postgres.communicate(statements.encode())[0].decode()
-        #
-        # ## create database and execute statements on sqlite with command line
-        # sqlite_command = f""" sqlite3.exe ./target/databases/{database_name}.db"""
-        # sqlite_console = subprocess.Popen(sqlite_command, shell=True, stdout=subprocess.PIPE, stdin=subprocess.PIPE)
-        # sqlite_result = sqlite_console.communicate(statements.encode())[0].decode()
-
-
-        ## create database and execute statements on mysql with python lib
-        mysql_list = list()
-        try:
-            mysql_cur.execute(f"CREATE DATABASE {database_name};")
-            mysql_con = mysql.connector.connect(user="root", password="root", database=database_name)
-            mysql_con.autocommit = True #not actually needed
-            mysql_cur = mysql_con.cursor()
-            mysql_cur.execute(statements, multi=True)
-            for cur in mysql_cur.execute(statements, multi=True):
-                pass
-            for select in select_statements:
-                mysql_cur.execute(select)
-                for res in mysql_cur.fetchall():
-                    mysql_list.append(res[0])
-                    print(res[0])
-        except mysql.connector.Error as e: #mysql stop executing after finding an error
-            print(f"\nMySQL error at {database_name}:")
-            print(e)
-
-        ## create database and execute statements on postgres with python lib
-        postgres_list = list()
-        try:
-            postgres_cur.execute(f"CREATE DATABASE {database_name};")
-            postgres_con = psycopg.connect(f"dbname={database_name} user=sqlancer password=sqlancer")
-            postgres_con.autocommit = True
-            postgres_cur = postgres_con.cursor()
-            postgres_cur.execute(statements)
-            for select in select_statements:
-                postgres_cur.execute(select)
-                for res in postgres_cur.fetchall():
-                    postgres_list.append(res[0])
-                    print(res[0])
-        except psycopg.Error as e: #postgres will not execute from the start if an error is detected
-            print(f"\nPostgreSQL error at {database_name}:")
-            print(e)
-
-        ## create database and execute statements on sqlite with python lib
-        sqlite_list = list()
-        try:
-            sqlite_con = sqlite3.connect(f"./target/databases/{database_name}.db")
-            sqlite_cur = sqlite_con.cursor()
-            res = sqlite_cur.executescript(statements)
-            for select in select_statements:
-                res = sqlite_cur.execute(select)
-                for tuple in res.fetchall():
-                    sqlite_list.append(tuple[0])
-                    print(tuple[0])
-        except sqlite3.Error as e: #sqlite stop executing after finding an error
-            print(f"\nSQLite error at {database_name}: ")
-            print(e)
-
-
-        # print(len(mysql_list) == len(postgres_list))
-        # print(len(sqlite_list) == len(postgres_list))
-        # len = len(sqlite_list)
-        # for i in range(len):
-        #     print(mysql_list[i] == postgres_list[i], sqlite_list[i] == postgres_list[i], mysql_list[i] == sqlite_list[i])
-
-        # mysql always escape \ but the other two do not. 'NO_BACKSLASH_ESCAPES' mode can disable it
+    print("Finish running")
