0d941cb0189be2aa6db3d1a13814772326e17ac6
Nathee Jaywaree <puntf2@gmail.com>
2023-12-01 02:51:15 +0700

    Category I04

--- src/sqlancer/postgres/gen/PostgresInsertGenerator.java
+++ src/sqlancer/postgres/gen/PostgresInsertGenerator.java
@@ -41,12 +41,12 @@ public final class PostgresInsertGenerator {
         sb.append("(");
         sb.append(columns.stream().map(c -> c.getName()).collect(Collectors.joining(", ")));
         sb.append(")");
-        if (Randomly.getBooleanWithRatherLowProbability()) {
-            sb.append(" OVERRIDING");
-            sb.append(" ");
-            sb.append(Randomly.fromOptions("SYSTEM", "USER"));
-            sb.append(" VALUE");
-        }
+        // if (Randomly.getBooleanWithRatherLowProbability()) {
+        //     sb.append(" OVERRIDING");
+        //     sb.append(" ");
+        //     sb.append(Randomly.fromOptions("SYSTEM", "USER"));
+        //     sb.append(" VALUE");
+        // }
         sb.append(" VALUES");
 
         if (globalState.getDbmsSpecificOptions().allowBulkInsert && Randomly.getBooleanWithSmallProbability()) {
